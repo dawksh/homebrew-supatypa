19675bc3a5bc2eb2246e98c510802ead50aec911
daksh <akshd@outlook.com>
2026-01-03 17:17:23 +0530

    chore: fix hash

--- Formula/supatypa.rb
+++ Formula/supatypa.rb
@@ -2,7 +2,7 @@ class Supatypa < Formula
   desc "macOS menu bar app that tracks daily typing statistics"
   homepage "https://github.com/dawksh/supatypa"
   url "https://github.com/dawksh/supatypa/releases/download/v0.1.1/supatypa"
-  sha256 "dd6f1d70dfc0d2c26eeb0837ecfebbc8e2afa8b4e9693fca6772081a872a0367"
+  sha256 "b37b310ed04f37b2ebc4442edd0add21769358ecfc992683ba1e8faffe4f1b12"
   license "MIT"
 
   depends_on :macos
